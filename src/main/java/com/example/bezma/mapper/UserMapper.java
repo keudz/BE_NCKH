7368dcc1a6dc963fa84b749f1e534df3e9a2d3ca
Quang <luciuss710@gmail.com>
2026-03-31 20:48:29 +0700

    ok

---
+++ src/main/java/com/example/bezma/mapper/UserMapper.java
@@ -0,0 +1,38 @@
+package com.example.bezma.mapper;
+
+import com.example.bezma.dto.req.user.UserRegistrationRequest;
+import com.example.bezma.dto.req.user.UserUpdateRequest;
+import com.example.bezma.dto.res.user.UserDetailResponse;
+import com.example.bezma.dto.res.user.UserSummaryResponse;
+import com.example.bezma.entity.user.User;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", constant = "PENDING_ACTIVE")
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "verificationToken", ignore = true)
+    @Mapping(target = "verificationTokenExpiry", ignore = true)
+    User toEntity(UserRegistrationRequest request);
+
+    @Mapping(target = "tenantId", source = "tenant.id")
+    @Mapping(target = "tenantCode", source = "tenant.tenantCode")
+    @Mapping(target = "roleName", source = "role.name")
+    @Mapping(target = "slug", ignore = true)
+    UserSummaryResponse toSummaryResponse(User user);
+
+    @Mapping(target = "avatarZalo", source = "avatar")
+    @Mapping(target = "subPhone", source = "phone")
+    UserDetailResponse toDetailResponse(User user);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "tenant", ignore = true)
+    @Mapping(target = "isActive", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    void updateUserFromRequest(UserUpdateRequest request, @MappingTarget User user);
+}
