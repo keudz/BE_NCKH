17727914ea83ff45dc8d6c90dbfa58bacb378a09
nguyenthedat <dnguyenthe706@gmail.com>
2026-04-08 02:08:59 +0700

    sua loi theo mau co san

--- src/main/java/com/example/bezma/service/impl/ZaloServiceImpl.java
+++ src/main/java/com/example/bezma/service/impl/ZaloServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.bezma.service.impl;
 
+import com.example.bezma.common.enumCom.ErrorCode;
+import com.example.bezma.exception.AppException;
 import com.example.bezma.service.iService.ZaloService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,21 +15,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class ZaloServiceImpl implements ZaloService {
 
+    @Override
     public String getZaloIdFromToken(String accessToken) {
         try {
             RestTemplate restTemplate = new RestTemplate();
             String url = "https://graph.zalo.me/v2.0/me";
+
             HttpHeaders headers = new HttpHeaders();
             headers.set("access_token", accessToken);
+
             HttpEntity<String> entity = new HttpEntity<>(headers);
             ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
             ObjectMapper mapper = new ObjectMapper();
             JsonNode root = mapper.readTree(response.getBody());
 
             return root.path("id").asText();
 
         } catch (Exception e) {
-            throw new RuntimeException("Zalo Token không hợp lệ hoặc đã hết hạn!");
+            throw new AppException(ErrorCode.INVALID_TOKEN);
         }
     }
 }
