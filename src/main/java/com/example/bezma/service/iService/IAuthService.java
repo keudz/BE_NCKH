7c91b9d0986e09a53d1097e09f1d237cb6aff72d
nguyenthedat <dnguyenthe706@gmail.com>
2026-04-08 01:43:52 +0700

    login bang zalo

--- src/main/java/com/example/bezma/service/iService/IAuthService.java
+++ src/main/java/com/example/bezma/service/iService/IAuthService.java
@@ -2,9 +2,12 @@ package com.example.bezma.service.iService;
 
 import com.example.bezma.dto.req.auth.LoginRequest;
 import com.example.bezma.dto.req.auth.RefreshTokenRequest; // Thêm dòng này
+import com.example.bezma.dto.req.auth.ZaloLoginRequest;
 import com.example.bezma.dto.res.auth.AuthResponse;
 
 public interface IAuthService {
     AuthResponse login(LoginRequest request);
     AuthResponse refreshToken(RefreshTokenRequest request); // Thêm dòng này
+    AuthResponse loginZalo(ZaloLoginRequest request);
+
 }
