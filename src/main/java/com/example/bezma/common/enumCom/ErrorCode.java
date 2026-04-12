d134bb9ae655d82a3b8722934283d9e6ddc7ca07
Quang <luciuss710@gmail.com>
2026-04-13 00:10:20 +0700

    feat: add assignRole

--- src/main/java/com/example/bezma/common/enumCom/ErrorCode.java
+++ src/main/java/com/example/bezma/common/enumCom/ErrorCode.java
@@ -33,7 +33,10 @@ public enum ErrorCode {
     USER_ALREADY_EXISTS(1005, "Tên đăng nhập đã tồn tại", HttpStatus.CONFLICT),
     EMAIL_ALREADY_EXISTS(1006, "Email đã được đăng ký trước đó", HttpStatus.CONFLICT),
     ROLE_NOT_FOUND(1009, "Vai trò (Role) không tồn tại trên hệ thống", HttpStatus.NOT_FOUND),
-    USER_NOT_ACTIVE(1013, "Tài khoản hiện đang bị khóa", HttpStatus.FORBIDDEN);
+    USER_NOT_ACTIVE(1013, "Tài khoản hiện đang bị khóa", HttpStatus.FORBIDDEN),
+    USER_NOT_EXISTED(1014, "Người dùng không tồn tại trong hệ thống", HttpStatus.NOT_FOUND),
+    USER_ALREADY_EMPLOYEE(1015, "Người dùng này đã là nhân viên", HttpStatus.BAD_REQUEST);
+
     private final int code;
     private final String message;
     private final HttpStatus statusCode;
