89463996a88ecaaeb5c701f5527ad1b10addeb64
nguyenthedat <dnguyenthe706@gmail.com>
2026-05-01 23:11:27 +0700

    add @EnableJpaRepositories

--- src/main/java/com/example/bezma/BezmaApplication.java
+++ src/main/java/com/example/bezma/BezmaApplication.java
@@ -2,10 +2,12 @@ package com.example.bezma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaRepositories
 public class BezmaApplication {
 
     public static void main(String[] args) {
